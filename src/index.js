1449a89e5007b52391de1b67af37de26729a4e8c
beeebeep <YouCanWriteMe@ya.ru>
2021-03-08 20:32:08 +0700

    fix: implement algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    let bracketsPairs = {};
+    for (var j = 0; j < bracketsConfig.length; j++) {
+        bracketsPairs[bracketsConfig[j][0]] = bracketsConfig[j][1];
+    }
+
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === bracketsPairs[stack[stack.length - 1]]) {
+            stack.pop();
+        } else {
+            stack.push(str.charAt(i));
+        }
+    }
+    return stack.length === 0;
+};
